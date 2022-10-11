f509ac7793a0db899a68f91a0e9dbb8b9fd9fc94
Gooey Code <gooeycode@gmail.com>
2022-10-11 09:28:58 +0100

    small edits

--- articles/views.py
+++ articles/views.py
@@ -17,7 +17,7 @@ class CommentGet(DetailView):
     model = Article
     template_name = "article_detail.html"
 
-    def get_contextdata(self, **kwargs):
+    def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['form'] = CommentForm()
         return context
